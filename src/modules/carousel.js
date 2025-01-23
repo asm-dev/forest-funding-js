de01ca5de248f4550ad6aac478682c44d5a638f9
asm-dev <anasalamancamontero@gmail.com>
2025-01-23 17:20:19 +0000

    fix carrousel behaviour

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -11,22 +11,25 @@ export const initCarousel = () => {
   const updateCarousel = () => {
     const offset = currentIndex * -imageWidth;
     container.style.transform = `translateX(${offset}%)`;
+    console.log("updato container con ofset -->", offset);
   };
 
   const handleResize = () => {
     imagesToShow = window.innerWidth < 768 ? 1 : 3;
-    const imageWidth = 100 / imagesToShow;
+    imageWidth = 100 / imagesToShow;
 
     images.forEach((img) => (img.style.flex = `0 0 ${imageWidth}%`));
     updateCarousel();
   };
 
   prevButton.addEventListener("click", () => {
+    console.log("paso por aqui");
     currentIndex = Math.max(currentIndex - 1, 0);
     updateCarousel();
   });
 
   nextButton.addEventListener("click", () => {
+    console.log("paso por el otro");
     currentIndex = Math.min(currentIndex + 1, images.length - imagesToShow);
     updateCarousel();
   });
